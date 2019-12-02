08dcbcf7b0eb52d9f975362607db465accb70edb
htetthuaung283 <htetthuaung283@gmail.com>
2019-12-03 05:23:47 +0630

    'if'

--- ifstatement.py
+++ ifstatement.py
@@ -1,10 +1,14 @@
 x = int(input('Please enter an integer: '))
+
 if x < 0:
 	x = 0
 	print('negative changed to zero')
+
 elif x == 0:
 	print('x is equal to zero')
+
 elif x == 1:
 	print('x is equal to 1')
+
 else:
 	print('more')
